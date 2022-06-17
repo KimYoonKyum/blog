ef7db72abfb1acb863ec21000786a292489ae22c
YoonKyeom Kim <newalpha7@naver.com>
2022-06-17 19:52:15 +0900

    commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import React, { FunctionComponent } from 'react'
+import { Link } from 'gatsby'
+
+const IndexPage: FunctionComponent = function () {
+  return (
+    <div>
+      <Link to={'/info/'}>To Info</Link>
+    </div>
+  )
+}
+
+export default IndexPage
