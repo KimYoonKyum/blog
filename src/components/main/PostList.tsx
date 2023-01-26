78bbf0b4714af0e47cb5e7c64bfdd01a5d2a3645
YoonKyeom Kim <newalpha7@naver.com>
2023-01-26 23:11:22 +0900

    무한 스크롤 구현

--- src/components/main/PostList.tsx
+++ src/components/main/PostList.tsx
@@ -1,7 +1,10 @@
-import React, { FunctionComponent, useMemo } from 'react'
+import React, { FunctionComponent } from 'react'
 import styled from '@emotion/styled'
 import PostItem from 'components/Main/PostItem'
 import { PostListItemType } from 'types/PostItem.types'
+import useInfiniteScroll, {
+  useInfiniteScrollType,
+} from 'hooks/useInfiniteScroll'
 
 export type PostType = {
   node: {
@@ -42,24 +45,32 @@ const PostList: FunctionComponent<PostListProps> = function ({
   posts,
   selectedCategory,
 }) {
-  const postListData = useMemo(
-    () =>
-      posts.filter(
-        ({
-          node: {
-            frontmatter: { categories },
-          },
-        }: PostListItemType) =>
-          selectedCategory !== 'All'
-            ? categories.includes(selectedCategory)
-            : true,
-      ),
-    [selectedCategory],
+  // const postListData = useMemo(
+  //   () =>
+  //     posts.filter(
+  //       ({
+  //         node: {
+  //           frontmatter: { categories },
+  //         },
+  //       }: PostListItemType) =>
+  //         selectedCategory !== 'All'
+  //           ? categories.includes(selectedCategory)
+  //           : true,
+  //     ),
+  //   [selectedCategory],
+  // )
+  const { containerRef, postList }: useInfiniteScrollType = useInfiniteScroll(
+    selectedCategory,
+    posts,
   )
   return (
-    <PostListWrapper>
-      {postListData.map(({ node: { id, frontmatter } }: PostListItemType) => (
-        <PostItem {...frontmatter} link="https://www.google.co.kr/" key={id} />
+    <PostListWrapper ref={containerRef}>
+      {postList.map(({ node: { id, frontmatter } }: PostListItemType) => (
+        <PostItem
+          {...frontmatter}
+          link="<https://www.google.co.kr/>"
+          key={id}
+        />
       ))}
     </PostListWrapper>
   )
